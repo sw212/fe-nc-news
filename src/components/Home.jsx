bdef0199ed566cb3139172f37226d0386f9e324c
sw212 <sw212@github.com>
2024-02-09 15:29:37 +0000

    feat: add featured article to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,36 @@
+import { API } from "../api";
+
+import { useState, useEffect } from "react";
+
+import ArticleCard from "./ArticleCard";
+
 export default function Home()
 {
+    const [article, setArticle] = useState(null);
+
+    useEffect(() => {
+        const fetchArticle = async () => {
+            try
+            {
+                const response = await API.get("/articles?limit=1");
+                setArticle(response.data.articles[0]);
+            }
+            catch(err)
+            {
+                setError(null);
+            }
+        }
+        fetchArticle();
+    }, []);
+
     return (
-        <div className="text-3xl font-bold underline">
-            Welcome!
-        </div>
+        <>
+            {article &&
+            <>
+                <h1 className="py-4 px-2 text-3xl">Featured Article:</h1>
+                <ArticleCard article={article}/>
+            </>
+            }
+        </>
     )
 }
